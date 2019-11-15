c1ab3252dfa8c1d9cfcbee8e5656afdffd7e33d1
Reid Sherman <reidsherman@gmail.com>
2019-11-14 18:25:27 -0800

    Updates

---
+++ precache-manifest.1bb0d073c3cf96a49d4f359f4b22e046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75972a407f0f92a53ddd016a778a9d88",
+    "url": "/captest/index.html"
+  },
+  {
+    "revision": "ae8b6d4968eba396f9ba",
+    "url": "/captest/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "542ccc512f024bffb9bb",
+    "url": "/captest/static/js/2.91e6b2cd.chunk.js"
+  },
+  {
+    "revision": "ae8b6d4968eba396f9ba",
+    "url": "/captest/static/js/main.8e7360de.chunk.js"
+  },
+  {
+    "revision": "e16a5962ce0ce902c0a1",
+    "url": "/captest/static/js/runtime-main.fb3e692b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/captest/static/media/logo.25bf045c.svg"
+  }
+]);
